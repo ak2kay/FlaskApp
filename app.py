da5976a7d17b5f2ddc6833b1bea9ad0beda3d585
bryceyang <yandli@hotmail.com>
2018-03-22 10:41:31 +0800

    view

---
+++ app.py
@@ -0,0 +1,197 @@
+from functools import wraps
+
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from passlib.hash import sha256_crypt
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from forms import RegisterForm, ArticleForm
+
+app = Flask(__name__)
+
+#config mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'yssa'
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.secret_key = 'secret123'
+
+# init mysql
+mysql =  MySQL(app)
+
+def is_logged_in(f):
+    """confirm that if the request user is login """
+    @wraps(f)
+    def wrap(*args, **kwagrs):
+        if 'logged_in' in session:
+            return f(*args, **kwagrs)
+        else:
+            flash("Unauthorized, Please login", 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    # mysql
+    cur = mysql.connection.cursor()
+
+    result = cur.execute('SELECT * FROM articles')
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('articles.html', msg=msg)
+    return render_template('articles.html', articles=articles)
+
+@app.route('/articles/<string:id>/')
+def article(id):
+    cur = mysql.connection.cursor()
+
+    resutl = cur.execute('SELECT * FROM articles WHERE id = %s', [id])
+    article = cur.fetchone()
+    return render_template('article.html', article=article)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        #create cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password) )
+
+        mysql.connection.commit()
+
+        cur.close()
+        flash('You are now registerd and can log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        cur = mysql.connection.cursor()
+
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    cur = mysql.connection.cursor()
+
+    result = cur.execute('SELECT * FROM articles')
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('dashboard.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('dashboard.html', msg=msg)
+    return render_template('dashboard.html')
+
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('Articles create', 'success')
+
+        return redirect(url_for('dashboard'))
+    return render_template('add_article.html', form=form)
+
+#Edit Article
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+    cur = mysql.connection.cursor()
+    # get article by id
+    result = cur.execute('SELECT * FROM articles WHERE id=%s', [id])
+
+    article = cur.fetchone()
+    # get form
+    form = ArticleForm(request.form)
+    form.title.data = article['title']
+    form.body.data = article['body']
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        cur = mysql.connection.cursor()
+
+        cur.execute('UPDATE articles SET title=%s, body=%s WHERE id=%s', (title, body, id))
+
+        mysql.connection.commit()
+
+        cur.close()
+        flash('Article Edited', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('edit_article.html', form=form)
+
+# delete article
+@app.route('/delete_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def delete_article(id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM articles WHERE id=%s", [id])
+    mysql.connection.commit()
+    cur.close()
+    flash('Article Deleted', 'success')
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
